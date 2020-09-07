4e86a70422600c6041c4a4153135d92faee4be43
mhayesm <michael.hayes@stockport.gov.uk>
2020-09-07 16:51:12 +0100

    chore(mapper): change addressline3 to city

--- src/Mappers/DroppedKerbMapper.cs
+++ src/Mappers/DroppedKerbMapper.cs
@@ -30,7 +30,7 @@ namespace dropped_kerb_service.Mappers
                     {
                         AddressLine1 = model.CustomersAddress.AddressLine1,
                         AddressLine2 = model.CustomersAddress.AddressLine2,
-                        AddressLine3 = model.CustomersAddress.Town,
+                        City = model.CustomersAddress.Town,
                         Postcode = model.CustomersAddress.Postcode,
                         Reference = model.CustomersAddress.PlaceRef,
                         Description = model.CustomersAddress.ToString()
